4730b51e8a4bd6efa80caaf75537a20dfd1e6622
bogdant14@mail.ru <bogdant14@mail.ru>
2026-07-20 21:25:48 +0300

    TODO: place Factorial result

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -1,11 +1,14 @@
 // JavaScript source code
-function Factorial() {
+function Factorial()
+{
     let numberElement = document.getElementById("number");
     let number = numberElement.value;
     let resultElement = document.getElementById("factorial-result");
-    let f = 1;
-    for (let i = 1; i <= number; i++) {
+    let f = BigInt(1);
+    for (let i = 1n; i <= number; i++)
+    {
         f *= i;
     }
-    resultElement.innerHTML = f;
+    //resultElement.value = `${number}! = ${f}`;
+    resultElement.innerHTML = `${number}! = ${f}`;
 }
